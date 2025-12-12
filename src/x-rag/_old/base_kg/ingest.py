3aadb5ce34a90f1360c987a55f0896c77a728557
nathan inbar <nathaninbar@gmail.com>
2025-12-11 21:19:06 -0800

    move old ingest code wip to _old folder

--- src/x-rag/_old/base_kg/ingest.py
+++ src/x-rag/_old/base_kg/ingest.py
@@ -1,37 +1,44 @@
 import asyncio
 import sys
 import json
-import dspy
+import logging
 
 from pathlib import Path
 ROOT = Path(__file__).resolve().parents[1]
 print(ROOT)
 sys.path.insert(0, str(ROOT))
 
+from utils.models import subject, predicate, obj
 from utils.models import Chunk
 from utils.db import ChunkDatabase
 from utils.segmenter import TextSegmenter
+from base_kg.extract_dspy import TripletExtractor
+from dotenv import load_dotenv
 
-DATASET_DIR = "datasets"
+# config
+TEXT_SEGMENTER_MODEL = "sat-3l-sm"
+TRIPLET_EXTRACTOR_MODEL = "bedrock/us.amazon.nova-pro-v1:0"
+EMBED_MODEL = "bedrock/amazon.titan-embed-text-v2:0"
+
+DATASET = "MINE"
+AWS_REGION = "ca-central-1"
+SECRETS_ENV = "../../secrets.env"
+
+# internal global
+_DATASET_DIR = "datasets"
 
 segmenter = None
+triplet_extractor = None
 
-class ExtractTriples(dspy.Signature):
-    """
-    Extract subject predicate object triples from the source text.
-    Be thorough, accurate, and faithful to the source text.
-    Return a JSON array under 'triples_json' with objects:
-      {"subject": "...", "predicate": "...", "object": "..."}
-    """
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-    source_text = dspy.InputField()
-    triples_json = dspy.OutputField(desc="JSON array of {subject,predicate,object}")
 
 async def ingestion_task(file_path:Path):
     """
     Ingest a single json document into the graph
     """
-    print(f"ingesting file '{file_path}'")
+    print(f"ingesting file '{file_path}' ..")
 
     # expect json object 'essay' key:
     with open(file_path, "r") as file:
@@ -42,10 +49,11 @@ async def ingestion_task(file_path:Path):
         
     dataset_text = data['essay']
     chunk_texts = segmenter.create_segments(dataset_text)
+    logger.debug(f"CHUNKS: \n{chunk_texts}\n")
 
     for txt in chunk_texts:
-        ... #todo - call dspy extraction , upsert to memgraph , ..
-
+        triplets: list[tuple[subject,predicate,obj]] = await triplet_extractor.extract(txt)
+        logger.debug(f"\nTRIPLETS: \n{triplets}\n")
     # get embedding for chunk text
 
     # assemble chunk object & add to chunk database
@@ -57,18 +65,26 @@ async def ingest_dataset(dataset_folder_name:str) -> None:
     in memgraph with chunk data in a ChunkDatabase
     """
     base:Path = Path(__file__).resolve()
-    target:Path = base.parents[3] / DATASET_DIR /dataset_folder_name
+    target:Path = base.parents[3] / _DATASET_DIR /dataset_folder_name
 
     ingest_tasks:list[asyncio.Task] = []
     for p in target.iterdir():
         if p.is_file() and str(p).endswith(".json"):
             # create an ingestion task
             ingest_tasks.append(asyncio.create_task(ingestion_task(p)))
+            break # TEMP : only ingest 1 document
 
     await asyncio.gather(*ingest_tasks)
     print("Ingestion of dataset complete.")
 
 
 if __name__ == "__main__" :
+
+    load_dotenv(SECRETS_ENV)
+
     segmenter = TextSegmenter()
-    asyncio.run(ingest_dataset("MINE"))
+    segmenter.configure(model= TEXT_SEGMENTER_MODEL)
+
+    triplet_extractor = TripletExtractor(model= TRIPLET_EXTRACTOR_MODEL)
+
+    asyncio.run(ingest_dataset(DATASET))
