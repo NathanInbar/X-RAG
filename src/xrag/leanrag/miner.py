637154fca05be2b4198f5ecb861379d54a987f75
nathan inbar <nathaninbar@gmail.com>
2026-02-02 18:58:02 -0800

    working on path cleanup

---
+++ src/xrag/leanrag/miner.py
@@ -0,0 +1,22 @@
+class LeanragMINER(MINER):
+    def __init__(self):
+        # tracks how many ingested articles were too small to construct a 'proper' leanrag graph
+        # i.e had to fall back since recursion stopped at layer 0
+        self.small_articles = set()
+
+    async def ingest(self, preprocess_chunks_filepath:Path, preprocess_descriptions_filepath:Path):
+        # create base KG
+        await upsert_from_preprocessed(preprocess_chunks_filepath, preprocess_descriptions_filepath)
+        self.small_articles.clear()
+
+    async def pre_retrieve(self, article_name):
+        # build LeanRAG KG with hierarchical clustering
+        last_layer = await build()
+        if last_layer == 0:
+            self.small_articles.add(article_name)
+    
+    async def retrieve(self, text, preprocess_chunks_filepath:Path):
+        return "\n".join(await leanrag_retrieve.get_response_context_data(text, preprocess_chunks_filepath))
+    
+    async def reset(self):
+        await mg_driver.clear()
