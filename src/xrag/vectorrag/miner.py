81a92be5f1295130e9430cd1403a9f37f85e6cde
Saskapult <thekieransmith@gmail.com>
2026-02-25 20:20:10 -0800

    vector RAG with preprocessed data

--- src/xrag/vectorrag/miner.py
+++ src/xrag/vectorrag/miner.py
@@ -7,39 +7,44 @@ from pathlib import Path
 import ijson
 
 class BasicVectorMINER(MINER):
-	""" A MINER implementation for a very simple vector RAG system. """
+	""" 
+	A MINER implementation for a very simple vector RAG system. 
+	
+	This implementation relies on externally processed chunk data. 
+	"""
 
 	chunks: list[str] = []
 	embeddings: list = []
 
 	def __init__(
 		self,
-		chunk_size: int = 200,
-		overlap: int = 20,
+		# chunk_size: int = 200,
+		# overlap: int = 20,
 		quantile: float = 0.95
 	):
-		self.chunk_size = chunk_size
-		self.overlap = overlap
+		# self.chunk_size = chunk_size
+		# self.overlap = overlap
 		self.quantile = quantile 
 		self.embedding_model = config.models["embed"]
 
 	async def ingest(self, chunks: Path, descriptions: Path):
 		with open(chunks, "rb") as in_file:
-			for itm in ijson.items(in_file, "item"):
-				for i,chunk in enumerate(itm['chunks']):
-					text = chunk["raw_text"]
+			for item in ijson.items(in_file, "item"):
+				for chunk in enumerate(item["chunks"]):
+					self.chunks.append(chunk["raw_text"])
+					self.embeddings.append(np.array(chunk["embedding"]))
 
-					new_chunks = [text[i*self.chunk_size:(i+1)*self.chunk_size+self.overlap] for i in range(0, len(text)//self.chunk_size)]
-					new_embeddings = await embed_call_with_retry(new_chunks)
-					new_embeddings = [np.array(e.embedding) for e in new_embeddings.data]
+					# new_chunks = [text[i*self.chunk_size:(i+1)*self.chunk_size+self.overlap] for i in range(0, len(text)//self.chunk_size)]
+					# new_embeddings = await embed_call_with_retry(new_chunks)
+					# new_embeddings = [np.array(e.embedding) for e in new_embeddings.data]
 
-					self.chunks += new_chunks
-					self.embeddings += new_embeddings
+					# self.chunks += new_chunks
+					# self.embeddings += new_embeddings
 	
 	async def pre_retrieve(self, article_name):
 		pass
 
-	async def retrieve(self, query_text, preprocess_chunks_filepath:Path) -> str:
+	async def retrieve(self, query_text, preprocess_chunks_filepath: Path) -> str:
 		# Make embedding 
 		text_embedding = (await litellm.aembedding(self.embedding_model, input=[query_text]))
 		text_embedding = np.array(text_embedding.data[0].embedding)
